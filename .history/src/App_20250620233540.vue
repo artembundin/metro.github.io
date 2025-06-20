3ca7714e2dff5d9922d5c86100ca6702e4508401
Artem Barancheev <artembarancheev@MacBook-Air-Artem-2.local>
2025-06-21 01:37:12 +0300

    first commit

---
+++ .history/src/App_20250620233540.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" v-if="linesData && stationsData">
+    <MetroMap/>
+    <ChildComponent :stations="stationsData" :lines="linesData" />
+  </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vu'
+import metroData from './assets/metroData.json'
+
+
+
+export default {
+  components: {
+    ChildComponent,
+  },
+  data() {
+    return {
+      stationsData: [],
+      linesData: [],
+    }
+  },
+  created() {
+    console.log(metroData)
+    console.log(metroData.lines)
+    this.linesData = metroData.lines
+    this.stationsData = metroData.stations
+  }
+}
+</script>
